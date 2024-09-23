b55a09af7e353613e5a99436e5f26116143ea400
deepadangi01 <dangideepa1999@gmail.com>
2024-09-22 21:24:18 -1200

    create file

--- project/app/urls.py
+++ project/app/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from app import views;
 
 urlpatterns = [
-    path('home',views.home,name="home"),
+    path('',views.home,name="home"),
     path('register/',views.register,name="register"), 
     path('login/',views.login, name="login"),  
-    
+    path('rdata/',views.rdata,name="rdata"),    
 ]
--- project/app/views.py
+++ project/app/views.py
@@ -10,21 +10,25 @@ def register(request):
 def login(request):
     if request.method=='POST':
         email = request.POST.get('email')
-        password = request.POST.get('password')
-        name1=request.COOKIES['name'] 
-        contact1=request.COOKIES['contact']
-        email1=request.COOKIES['email'] 
-        password1=request.COOKIES['password'] 
-        print(name1,contact1,email,password1)
+        password = request.POST.get('pass')
+        name1 = request.COOKIES['name'] 
+        contact1 = request.COOKIES['contact']
+        date1 = request.COOKIES['date']
+        email1 = request.COOKIES['email'] 
+        password1 = request.COOKIES['pass'] 
+        course1= request.COOKIES['course']
+        print(name1,contact1,date1,email,password1)
         if email==email1:
-            if password1==password:
+            if password==password1:
                 data={
                     'name':name1,
-                    'email':email1,
                     'contact':contact1,
-                    'pass':password1
+                    'date':date1,
+                    'email':email1,
+                    'pass':password1,
+                    'course':course1
                     }
-                return render(request,'dashboard.html',data)
+                return render(request,'index.html',data)
                 # return render(request,'first.html',data)
             else:
                 msg="email & password not matched "
@@ -37,25 +41,30 @@ def login(request):
        msg="Welcome to login page"
        return render(request,'login.html',{'msg':msg})
 
-# def rdata(request):
-#     print(request.method)
-#     print(request.POST)
-#     cstoken = request.POST.get('csrfmiddlewaretoken')
-#     name = request.POST.get('nm')
-#     contact = request.POST.get('con')
-#     email = request.POST.get('em')
-#     password = request.POST.get('pass')
-#     # print(cstoken)
-#     # print(name)
-#     # print(contact)
-#     # print(email)
-#     # print(password)
-#     response = render(request,'login.html')
-#     response.set_cookie('name',name)
-#     response.set_cookie('contact',contact)
-#     response.set_cookie('email',email)
-#     response.set_cookie('password',password)
-#     return response
+def rdata(request):
+    print(request.method)
+    print(request.POST)
+    cstoken = request.POST.get('csrfmiddlewaretoken')
+    name = request.POST.get('nm')
+    contact = request.POST.get('con')
+    date = request.POST.get('dt')
+    email = request.POST.get('em')
+    password=request.POST.get('pass')
+    course=request.POST.get('course')
+    
+    # print(cstoken)
+    # print(name)
+    # print(contact)
+    # print(email)
+    # print(password)
+    response = render(request,'login.html')
+    response.set_cookie('name',name)
+    response.set_cookie('contact',contact)
+    response.set_cookie('date',date)
+    response.set_cookie('email',email)
+    response.set_cookie('pass',password)
+    response.set_cookie('course',course)
+    return response
 # def userlogin(request):
 #     if request.method=='POST':
 #         email = request.POST.get('email')
