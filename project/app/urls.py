1e900fc7982ad654dda9f53664fecfd7b01717a7
deepadangi01 <dangideepa1999@gmail.com>
2024-10-17 23:29:57 -1200

    change design

--- project/app/urls.py
+++ project/app/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from app import views;
 
 urlpatterns = [
-    path('',views.register,name="register"), 
-    path('home/',views.home,name="home"),
+    path('',views.home,name="home"),
+    path('register/',views.register,name="register"), 
     path('login/',views.login, name="login"),  
     path('logout/',views.logout,name="logout"),
     path('query',views.query,name="query"),
